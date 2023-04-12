7e8245841dda45693d6c2f803a0e2becaf3e8b99
Phynomo <phynomo09099@gmail.com>
2023-04-12 00:02:57 -0600

    MINI LOGIN

---
+++ base_flutter/lib/screens/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../src/constants/image_strings.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key) ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Color.fromRGBO(255, 211, 0, 1),
+      body: SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(1),
+        child: Column(
+          children: [
+            //Image(image: const AssetImage(logo2)),
+            Image.asset('assets/images/Logo2.png'),
+            Text("¡Bienvenido a nuestra aplicación!"),
+            Text("inicia o crea sesión para disfrutar de los mejores viajes")
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
